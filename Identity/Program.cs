01d72c30c79f35465b2f001d8e1931c8cf34fa83
Yourbestfriendzxc <cyloev9@gmail.com>
2022-03-19 12:56:12 +0300

    Normal auth

--- Identity/Program.cs
+++ Identity/Program.cs
@@ -1,9 +1,19 @@
 using Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+UserStore s = new UserStore();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-User u = new User();
-u.Role = Role.Support;
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.MapPost("/auth", (AuthBody u) =>s.Authorizate(u.Login, u.Password));
+app.MapPost("/register", ((User u) => s.Register(u.Login,u.Password,u.Role)));
+app.UseStaticFiles();
 app.Run();
--- Identity/User.cs
+++ Identity/User.cs
@@ -9,7 +9,7 @@ public class User
     public Role Role { get; set; }
 }
 
-public class UserStore : IEnumerable
+public class UserStore
 {
     private List<User> userList;
 
@@ -17,8 +17,29 @@ public class UserStore : IEnumerable
     {
         userList = new List<User>();
     }
-    public IEnumerator GetEnumerator() => userList.GetEnumerator();
+
+    public void Register(string login,string password,Role role)
+    {
+        if(!userList.Exists(user=>user.Login==login))userList.Add(new User()
+        {
+            Login=login,
+            Password=password,
+            Role=role
+        });
+    }
+
+    public User Authorizate(string login,string password) {
+        var user = userList.Find(user => user.Login == login);
+        if (user!=null&&user.Password==password) return user;
+        return null;
+    }
 }
+public class AuthBody{
+    public string Login { get; set; }
+    public string Password { get; set; }
+    
+}
+
 public enum Role{
     Administrator,Customer,Support
 }
